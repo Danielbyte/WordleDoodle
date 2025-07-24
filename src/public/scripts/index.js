71d2ef438d7463c75a5099369bb2f65fe9c9305b
Danielbyte <daniel.sifiso.masango@gmail.com.com>
2025-07-24 02:47:30 +0200

    Added functionality to capture player input from the keyboard

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   createSquares();
+  keyPressEventHandler();
 })
 
 function createSquares() {
@@ -12,3 +13,16 @@ function createSquares() {
       gameBoard.appendChild(square);
     }
   }
+
+  function keyPressEventHandler() {
+    const keys = document.querySelectorAll('.keyboard-row button');
+    for (let i = 0; i < keys.length; i++) {
+      keys[i].onclick = ({target}) => {
+        const key = target.getAttribute('data-key');
+      }  
+    }
+  }
+
+function updateGuessedWords(letter) {
+  
+}
