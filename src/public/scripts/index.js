7ab3557355c7826edf8c099a7b24aa1b3ea92454
Danielbyte <daniel.sifiso.masango@gmail.com.com>
2025-07-25 02:12:17 +0200

    Word too short and win condition

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,3 +1,6 @@
+const dailyWord = 'Daily'; //dummy word that will be replaced by a proper word
+let guessedword = '';
+
 document.addEventListener("DOMContentLoaded", () => {
   createSquares();
   keyPressEventHandler();
@@ -20,20 +23,42 @@ function createSquares() {
     for (let i = 0; i < keys.length; i++) {
       keys[i].onclick = ({target}) => {
         const key = target.getAttribute('data-key');
-        updateGuessedWords(key);
+        switch(key)
+        {
+          case 'enter':
+            handleEnterKeyPress();
+            break;
+          case 'del':
+            console.log('Delete last letter');
+            break;
+          default:
+            updateGuessedWords(key);
+        }
       }  
     }
   }
 
+  function handleEnterKeyPress()
+  {
+    if (guessedword.length < 5)
+      console.log('Word too short!!!');
+    
+    if (guessedword.toLowerCase() === dailyWord.toLowerCase())
+      console.log('You win!!!');
+  }
+
 function updateGuessedWords(letter) {
   //Iterate throuh each square block of the game board and assign letter to corresponding board square
+  guessedword = '';
   let squares = document.querySelectorAll('.square');
   for(let i = 0; i < squares.length; i++)
   {
     if (squares[i].textContent.trim() === '')
     {
       squares[i].textContent = letter;
+      guessedword += letter;
       break;
     }
+    guessedword += squares[i].textContent.trim();
   }
 }
