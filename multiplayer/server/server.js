d50b6a3128301c3d04d00122e3948a82a427b0ec
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-15 01:44:43 +0200

    (Multiplayer-mode): Fix create server import

--- multiplayer/server/server.js
+++ multiplayer/server/server.js
@@ -1,9 +1,9 @@
 import { Server } from 'socket.io';
-import { createServer } from 'https';
-const httpsServer = createServer();
+import { createServer } from 'http';
+const httpServer = createServer();
 import { PORT } from '../../config/env.js'; //The port from environment variables
 
-const io = new Server(httpsServer, {
+const io = new Server(httpServer, {
   cors: {
     //In the event that the website url is know, will have multiple cors here => origin [PORT, https://appDomainUrl.com]
     //For now, the following environment variable is enough
