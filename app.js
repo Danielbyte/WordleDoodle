91b82780f618d1b0df38f5fec0728cbf267256af
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-15 23:19:15 +0200

    Direct import of dirname and join modules

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { PORT } from './config/env.js';
-import path from 'path'
+import { dirname, join} from 'path'
 
 import { fileURLToPath } from 'url';
 
 // Recreate __filename and __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -18,7 +18,7 @@ import wordValidationRouter from './single-player/src/routes/wordValidationRoute
 //MIDDLEWARE
 app.use(express.json());
 //Tell express to serve the files from the public directory as static files
-app.use(express.static(path.join(__dirname, './single-player/src/public')));
+app.use(express.static(join(__dirname, './single-player/src/public')));
 
 //Mount routes
 app.use('/',singlePlayerRouter); //We want to call the landing page instead (but for now will just serve the single player router)
@@ -27,7 +27,7 @@ app.use('/api/v1/validate', wordValidationRouter);
 app.use('/api/v1/verify', wordValidationRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server started on: http://localhost:${PORT}`);
+  console.log(`Server listening on PORT: ${PORT}`);
 });
 
 export default app;
