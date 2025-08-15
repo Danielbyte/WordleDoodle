dbf0a73223404ea1f5422198385de5a168979663
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-15 02:00:28 +0200

    (multiplayer-mode): Added client functions for user to join or create a room

--- multiplayer/client/public/scripts/client.js
+++ multiplayer/client/public/scripts/client.js
@@ -8,3 +8,15 @@ const SERVER_URL = PORT !== '.env.development.local' ? 'https://wordleDoodle.com
 const socket = io(SERVER_URL, {
   withCredentials: true
 });
+
+//Host wants to create a room
+//Pass in the username. To prevent the room information from being exposed in the front end, room creation will be handled by the server
+function createRoom(userName) {
+  socket.emit('createRoom', userName);
+}
+
+//User is joining a room
+//User needs to to pass in the room id they ought to join and their username
+function joinRoom(roomID, userName) {
+  socket.emit('joinRoom', roomID, userName);
+}
