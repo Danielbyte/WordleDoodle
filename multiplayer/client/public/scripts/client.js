977c4e223472476dcd714ee34543dc33f9876f42
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-16 23:29:13 +0200

    Removed unused code from client.js

--- multiplayer/client/public/scripts/client.js
+++ multiplayer/client/public/scripts/client.js
@@ -1,15 +1,6 @@
-//import { io } from 'socket.io-client'
-
-//const PORT = process.env.PORT || 5500;
-
-//const SERVER_URL = PORT; //!== '.env.development.local' ? 'https://wordleDoodle.com' : PORT; //if not development, default to production => app link
-
-/*
-//Instantiate client side socket
-const socket = io(SERVER_URL, {
-  withCredentials: true
-});*/
+//This client file will be served by Express server (app.js)
 const socket = io();
+
 //Host wants to create a room
 //Pass in the username. To prevent the room information from being exposed in the front end, room creation will be handled by the server
 function createRoom(userName) {
