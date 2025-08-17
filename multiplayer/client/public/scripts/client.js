42556d283ec8786543aac68d6aa33af8c7ccee47
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-17 17:30:32 +0200

    Added create_room and join_room event listeners

--- multiplayer/client/public/scripts/client.js
+++ multiplayer/client/public/scripts/client.js
@@ -3,13 +3,13 @@ const socket = io();
 //Host wants to create a room
 //Pass in the username. To prevent the room information from being exposed in the front end, room creation will be handled by the server
 function createRoom(userName) {
-  socket.emit('createRoom', userName);
+  socket.emit('create_room', userName);
 }
 
 //User is joining a room
 //User needs to to pass in the room id they ought to join and their username
 function joinRoom(roomID, userName) {
-  socket.emit('joinRoom', roomID, userName);
+  socket.emit('join_room', roomID, userName);
 }
 
 document.getElementById('createRoom').onclick = () => {
