9e22a09e5ec0d9751755eb53b3e450dffd2a434f
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-24 22:29:45 +0200

    Updated multiplayerRoutes file name

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const io = new Server(server, {
 
 //Load routes
 import singlePlayerRouter from './single-player/src/routes/singlePlayerMainRoutes.js';
-import multiplayerRouter from './multiplayer/src/routes/multiPlayerMainRoutes.js';
+import multiplayerRouter from './multiplayer/src/routes/multiPlayerRoutes.js';
 import wordValidationRouter from './single-player/src/routes/wordValidationRoute.js';
 import handleSocketEvent from './sockets/webSocketServer.js';
 
