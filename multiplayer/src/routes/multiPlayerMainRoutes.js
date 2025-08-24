0573958d4dc0366a4c1badabf9bd3469e5bfdf3c
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-24 22:20:00 +0200

    Renamed multiplayerMainRouter module to multiplayerRouter

--- multiplayer/src/routes/multiPlayerMainRoutes.js
+++ multiplayer/src/routes/multiPlayerMainRoutes.js
@@ -1,24 +1,24 @@
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { Router } from 'express';
-const multiplayerMainRouter = Router();
+const multiplayerRouter = Router();
 
 //Get file path from the URL of the current module
 const __fileName = fileURLToPath(import.meta.url);
 //Get the directory name from the file path
 const __dirname = dirname(__fileName);
 
-multiplayerMainRouter.get('/', (req, res) => {
+multiplayerRouter.get('/', (req, res) => {
   res.send('Multiplayer route');
 })
 
 //Serve the main menu of multiplayer
-multiplayerMainRouter.get('/multiplayerMain', (req, res) => {
+multiplayerRouter.get('/multiplayerMain', (req, res) => {
   res.sendFile(join(__dirname, '../../client/public/views', 'mainMenu.html'));
 });
 
-multiplayerMainRouter.get('/player-board', (req, res) => {
+multiplayerRouter.get('/player-board', (req, res) => {
   res.sendFile(join(__dirname, '../../client/public/views', 'board.html'))
 })
 
-export default multiplayerMainRouter;
+export default multiplayerRouter;
