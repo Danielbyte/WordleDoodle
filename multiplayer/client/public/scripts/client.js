2fabb99a739f3e075231dbce536075a1cd8bf9f8
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-26 21:10:15 +0200

    Multiplayer host board can be displayed after main menu page
    The main menu page consist of fields where user can create the room

--- multiplayer/client/public/scripts/client.js
+++ multiplayer/client/public/scripts/client.js
@@ -1,5 +1,8 @@
 //This client file will be served by Express server (app.js)
 const socket = io();
+let isInitialised = false;
+
+initialiseBoard();
 
 document.getElementById('createRoom').onclick = () => {
   const userName = document.getElementById('username').value;
@@ -67,3 +70,75 @@ socket.on('message', (payload) => {
   let data = JSON.parse(payload);
   console.log(data);
 });
+
+function initialiseBoard() {
+  if (isInitialised)
+    return;
+ 
+  //Add the main menu page
+  addUserNameTestField();
+  addCreateRoomButton();
+
+  isInitialised = true;
+}
+
+function addUserNameTestField() {
+  let mainMenu = document.getElementById('main-menu');
+
+    //Create username field and create room button
+  let usernameField = document.createElement('input');
+  usernameField.type = 'text';
+  usernameField.classList.add('username');
+  usernameField.name = 'username';
+  usernameField.id = 'user-name';
+  usernameField.placeholder = 'Enter your username';
+  mainMenu.appendChild(usernameField);
+
+  mainMenu.appendChild(document.createElement('br'));
+  mainMenu.appendChild(document.createElement('br'));  
+}
+
+function addCreateRoomButton() {
+  //Get the main-menu tag
+  let mainMenu = document.getElementById('main-menu');
+  let createRoomBtn = document.createElement('button');
+  createRoomBtn.id = 'create-room';
+  createRoomBtn.innerText = 'Create Room';
+
+  //Add a click event listener to to the button
+  createRoomBtn.addEventListener('click', () => {
+    document.getElementById('main-menu').remove(); //Remove the main menu div => main menu page
+    loadGameBoardContainer(); //Load multiplayer game loayout with all the boards
+  })
+
+  mainMenu.appendChild(createRoomBtn);
+}
+
+function loadGameBoardContainer() {
+  let gameBoardContainer = document.createElement('div');
+  gameBoardContainer.classList.add('game-board-container');
+  
+  //Add Player boards
+  let mainBoard = document.createElement('div');
+  mainBoard.classList.add('main-board');
+  gameBoardContainer.appendChild(mainBoard); //Should probably assign the username as the board id...
+
+  //The rest of the boards
+  let board1 = document.createElement('div');
+  board1.classList.add('board1');
+  gameBoardContainer.appendChild(board1);
+
+  let board2 = document.createElement('div');
+  board2.classList.add('board2');
+  gameBoardContainer.appendChild(board2);
+
+  let board3 = document.createElement('div');
+  board3.classList.add('board3');
+  gameBoardContainer.appendChild(board3);
+
+  let board4 = document.createElement('div');
+  board4.classList.add('board4');
+  gameBoardContainer.appendChild(board4);
+
+  document.body.appendChild(gameBoardContainer);
+}
