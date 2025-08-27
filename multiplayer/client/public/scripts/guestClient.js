c1bb2d64341579835ac0fa1147d7d85980e55a23
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-27 19:06:49 +0200

    Dynamically create username text field for guest user to enter their username

--- multiplayer/client/public/scripts/guestClient.js
+++ multiplayer/client/public/scripts/guestClient.js
@@ -5,19 +5,33 @@ displayGuestMainMenu();
 
 function displayGuestMainMenu() {
   addRoomCodeTextField();
+  addUserNameTextField();
 }
 
 function addRoomCodeTextField() {
-  let menuContainer = document.getElementById('guest-main-menu-container');
+  let menuContainer = document.querySelector('.guest-main-menu-container');
   let roomCodeTextField = document.createElement('input');
   roomCodeTextField.type = 'text';
   roomCodeTextField.name = 'room-code';
+  roomCodeTextField.id = 'room-code';
   roomCodeTextField.placeholder = 'Enter Room Code';
-  roomCodeTextField.appendChild(document.createElement('br'));
-  roomCodeTextField.appendChild(document.createElement('br'));
-  menuContainer.appendChild(addRoomCodeTextField);
+  menuContainer.appendChild(roomCodeTextField);
+
+  menuContainer.appendChild(document.createElement('br'));
+  menuContainer.appendChild(document.createElement('br'));
 }
 
 function addUserNameTextField() {
+  let menuContainer = document.querySelector('.guest-main-menu-container');
+  let userNameTextField = document.createElement('input');
+  userNameTextField.type = 'text';
+  userNameTextField.name = 'username';
+  userNameTextField.id = 'username';
+  userNameTextField.placeholder = 'Enter your Username';
+  userNameTextField.appendChild(document.createElement('br'));
+  userNameTextField.appendChild(document.createElement('br'));
+  menuContainer.appendChild(userNameTextField);
 
+  menuContainer.appendChild(document.createElement('br'));
+  menuContainer.appendChild(document.createElement('br'));
 }
