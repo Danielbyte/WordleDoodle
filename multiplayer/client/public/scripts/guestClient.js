8a4c5360e1abb3d0f2608b2ddb535393b3fb5143
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-27 18:33:14 +0200

    Dynamically create room code text  field for user to enter room code
    This is the main menu page of guest user

--- multiplayer/client/public/scripts/guestClient.js
+++ multiplayer/client/public/scripts/guestClient.js
@@ -1,1 +1,23 @@
 const socket = io();
+let username = '';
+
+displayGuestMainMenu();
+
+function displayGuestMainMenu() {
+  addRoomCodeTextField();
+}
+
+function addRoomCodeTextField() {
+  let menuContainer = document.getElementById('guest-main-menu-container');
+  let roomCodeTextField = document.createElement('input');
+  roomCodeTextField.type = 'text';
+  roomCodeTextField.name = 'room-code';
+  roomCodeTextField.placeholder = 'Enter Room Code';
+  roomCodeTextField.appendChild(document.createElement('br'));
+  roomCodeTextField.appendChild(document.createElement('br'));
+  menuContainer.appendChild(addRoomCodeTextField);
+}
+
+function addUserNameTextField() {
+
+}
