206096a78cf94726c64695bc5e584bb44cd37661
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-08-28 04:35:12 +0200

    Keyboard is displayed in guest board

--- multiplayer/client/public/scripts/guest-client.js
+++ multiplayer/client/public/scripts/guest-client.js
@@ -96,6 +96,7 @@ function displayGameBoard() {
   document.body.appendChild(gameBoardContainer);
   displayGuestBoard();
   createTiles();
+  addKeyBoard();
 }
 
 function displayGuestBoard() {
@@ -106,7 +107,11 @@ function displayGuestBoard() {
   board.id = 'board';
 
   boardContainer.appendChild(board);
-  mainBoard.appendChild(boardContainer);
+  let game = document.createElement('div');
+  game.id = 'game';
+
+  game.appendChild(boardContainer);
+  mainBoard.appendChild(game);
 }
 
 function createTiles() {
@@ -119,4 +124,50 @@ function createTiles() {
       tile.textContent = '';
       gameBoard.appendChild(tile);
     }
+  }
+
+  function addKeyBoard() {
+    let keyBoardContainer = document.createElement('div');
+    let keyBoard = `<div class="keyboard-row">
+          <button data-key="q">q</button>
+          <button data-key="w">w</button>
+          <button data-key="e">e</button>
+          <button data-key="r">r</button>
+          <button data-key="t">t</button>
+          <button data-key="y">y</button>
+          <button data-key="u">u</button>
+          <button data-key="i">i</button>
+          <button data-key="o">o</button>
+          <button data-key="p">p</button>
+        </div>
+        <div class="keyboard-row">
+          <div class="spacer-half"></div>
+          <button data-key="a">a</button>
+          <button data-key="s">s</button>
+          <button data-key="d">d</button>
+          <button data-key="f">f</button>
+          <button data-key="g">g</button>
+          <button data-key="h">h</button>
+          <button data-key="j">j</button>
+          <button data-key="k">k</button>
+          <button data-key="l">l</button>
+          <div class="spacer-half"></div>
+        </div>
+        <div class="keyboard-row">
+          <button data-key="enter" class="wide-button">Enter</button>
+          <button data-key="z">z</button>
+          <button data-key="x">x</button>
+          <button data-key="c">c</button>
+          <button data-key="v">v</button>
+          <button data-key="b">b</button>
+          <button data-key="n">n</button>
+          <button data-key="m">m</button>
+          <button data-key="del" class="wide-button">&#x232B;</button>
+        </div>`;
+
+    keyBoardContainer.innerHTML = keyBoard;
+
+
+    let game = document.getElementById('game');
+    game.appendChild(keyBoardContainer);
   }
