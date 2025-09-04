00ee75d5ab9d1df94ba0719ba98774c31d08d071
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-09-05 01:43:00 +0200

    Added user model schema

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 30
+  },
+  email: {
+    type: String,
+    required: [true, 'User email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, 'Please fill a valid email address']
+  },
+  password: {
+    type: String,
+    required: [true, 'User password is required'],
+    minLength: 8
+  }
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
