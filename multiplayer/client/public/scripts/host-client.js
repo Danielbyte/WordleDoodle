aa879fe4822319f4f1dc7e0eb771cfe1ac598bfc
Danielbyte <daniel.sifiso.masango@gmail.com>
2025-09-25 13:06:09 +0200

    Refactoring: Moved all html logic to html for host user page

--- multiplayer/client/public/scripts/host-client.js
+++ multiplayer/client/public/scripts/host-client.js
@@ -2,7 +2,6 @@
 const socket = io();
 let username = '';
 let roomcode = '';
-let maxOpponents = 4;
 const maxWordLength = 5; //Max word is 5 (Player guesses a five letter word)
 
 initialiseBoard();
@@ -103,7 +102,7 @@ function getTileColumn(row, tileIndex) {
 function initialiseBoard() {
   let hostBoard = document.querySelector('.main-board');
   hostBoard.style.gridRow = '1'; //The host board should have one row (for the host to set word and start game)
-  loadGameBoardContainer();
+  //loadGameBoardContainer();
 
   try {
     fetch('/multiplayer/username', {
@@ -115,7 +114,7 @@ function initialiseBoard() {
         if (data.username) {
           username = data.username;
           createRoom(username);
-          displayHostBoard();
+          //displayHostBoard();
         }
       })
   } catch (err) {
@@ -123,89 +122,19 @@ function initialiseBoard() {
   }
 }
 
-function loadGameBoardContainer() {
-  let gameBoardContainer = document.querySelector('.game-board-container');
+//function addSendMessageButtonEventListener() {
+document.getElementById('send-message-btn').addEventListener('click', () => {
+  let messageInputField = document.getElementById('message-input');
+  const message = messageInputField.value;
 
-  //The rest of the boards
-  let board1 = document.createElement('div');
-  board1.classList.add('board1');
-  gameBoardContainer.appendChild(board1);
+  if (!message) return; //If there is no message enetered, return (No need to send message)
 
-  let board2 = document.createElement('div');
-  board2.classList.add('board2');
-  gameBoardContainer.appendChild(board2);
-
-  let chat = document.createElement('div');
-  chat.classList.add('chat-section');
-  gameBoardContainer.appendChild(chat);
-
-  let board3 = document.createElement('div');
-  board3.classList.add('board3');
-  gameBoardContainer.appendChild(board3);
-
-  let board4 = document.createElement('div');
-  board4.classList.add('board4');
-  gameBoardContainer.appendChild(board4);
-
-  document.body.appendChild(gameBoardContainer);
-  createGuestBoards();
-  configureGuestBoards();
-  configureChat();
-}
-
-function configureChat() {
-  addChatHeading();
-  addChatField();
-  addInputField();
-}
-
-function addInputField() {
-  const chat = document.querySelector('.chat-section');
-
-  const messageInputSection = document.createElement('div');
-  messageInputSection.classList.add('message-input-section');
-  messageInputSection.id = 'message-input-section';
-
-  const relMessageSection = document.createElement('div');
-  relMessageSection.classList.add('rel-message-input-section');
-  relMessageSection.id = 'rel-message-input-section';
-
-  const inputField = document.createElement('input');
-  inputField.type = 'text';
-  inputField.id = 'message-input';
-  inputField.placeholder = 'Send message';
-  inputField.classList.add('chat-message-input');
-  relMessageSection.appendChild(inputField);
-  messageInputSection.appendChild(relMessageSection);
-
-  const sendButton = document.createElement('button');
-  sendButton.id = 'send-message-btn';
-  sendButton.classList.add('send-message-btn');
-  
-  const sendMsgIcon = document.createElement('img');
-  sendMsgIcon.src = '/cdn/images/send-msg.png'
-  sendMsgIcon.classList.add('send-msg-icon');
-  sendButton.appendChild(sendMsgIcon);
-  relMessageSection.appendChild(sendButton);
-  messageInputSection.appendChild(relMessageSection);
-
-  chat.appendChild(messageInputSection);
-  addSendMessageButtonEventListener();
-}
-
-function addSendMessageButtonEventListener() {
-  document.getElementById('send-message-btn').addEventListener('click', () => {
-    let messageInputField = document.getElementById('message-input'); 
-    const message = messageInputField.value;
-
-    if(!message) return; //If there is no message enetered, return (No need to send message)
-
-    const bubbleClass = 'left-bubble';
-    addMessageToChatUI(bubbleClass, message, username);
-    messageInputField.value = ''; //Clear the message field
-    broadcastMessageToRoom(message);
-  });
-}
+  const bubbleClass = 'left-bubble';
+  addMessageToChatUI(bubbleClass, message, username);
+  messageInputField.value = ''; //Clear the message field
+  broadcastMessageToRoom(message);
+});
+//}
 
 function addMessageToChatUI(bubbleClass, message, userName) {
   const messageContainer = document.getElementById('message-container');
@@ -236,78 +165,6 @@ function broadcastMessageToRoom(message) {
   }));
 }
 
-function addChatField() {
-  const chat = document.querySelector('.chat-section');
-  
-  const messageField = document.createElement('div');
-  messageField.classList.add('message-field');
-
-  //Unordered list to hold chats
-  const messageContainer = document.createElement('ul');
-  messageContainer.classList.add('message-container');
-  messageContainer.id = 'message-container';
-
-  messageField.appendChild(messageContainer);
-  chat.appendChild(messageField);
-}
-
-function addChatHeading() {
-  const chat = document.querySelector('.chat-section');
-
-  const heading = document.createElement('div');
-  heading.classList.add('heading');
-
-  const turtleImage = document.createElement('img');
-  turtleImage.src = '/cdn/images/tortoise.png';
-  turtleImage.classList.add('chat-turtle');
-  heading.appendChild(turtleImage);
-
-  const title = document.createElement('p');
-  title.classList.add('chat-title');
-  title.innerText = 'Chat';
-  heading.appendChild(title);
-
-  chat.appendChild(heading);
-}
-
-function createGuestBoards() {
-  //Create mini-boards for the other guest opponents
-  for (let i = 1; i <= maxOpponents; i++) {
-    createBoard(`board${i}`, `board-${i}`);
-  }
-}
-
-function createBoard(configBoard, configBoardId) {
-  let boardToBeConfigured = document.querySelector(`.${configBoard}`);
-  let boardContainer = document.createElement('div');
-  boardContainer.id = 'board-container';
-  let board = document.createElement('div');
-  board.id = configBoardId;
-
-  boardContainer.appendChild(board);
-  let game = document.createElement('div');
-  game.id = 'game';
-  game.appendChild(boardContainer);
-  boardToBeConfigured.appendChild(game);
-}
-
-function configureGuestBoards() {
-  for (let i = 1; i <= maxOpponents; i++)
-    configureBoard(`board-${i}`, `tile${i}`);
-}
-
-function configureBoard(boardId, tileClass) {
-  const gameBoard = document.getElementById(boardId);
-
-  for (let index = 0; index < 30; index++) {
-    const tile = document.createElement('div'); //Dynamically create a div tag
-    tile.classList.add(tileClass); //div tag class is a tile
-    tile.setAttribute('data-index', index + 1);
-    tile.textContent = '';
-    gameBoard.appendChild(tile);
-  }
-}
-
 // Function that allows the host to create a room
 function createRoom(username) {
   socket.emit('data', JSON.stringify({
@@ -317,58 +174,13 @@ function createRoom(username) {
   }));
 }
 
-function displayHostBoard() {
-  createWordOfTheDayTextField();
-  createStartGameButton();
-}
-
-function createWordOfTheDayTextField() {
-  let hostBoard = document.querySelector('.main-board');
-  let hostBoardContainer = document.createElement('div');
-  hostBoardContainer.classList.add('host-board-container');
-
-  //Create input section div
-  const wordSection = document.createElement('div');
-  wordSection.classList.add('word-input-section');
-
-  let wordOfTheDayTextField = document.createElement('input');
-  wordOfTheDayTextField.id = 'word-of-the-day';
-  wordOfTheDayTextField.name = 'word-of-the-day';
-  wordOfTheDayTextField.classList.add('word-of-the-day-input');
-  wordOfTheDayTextField.placeholder = 'Set word';
-  wordSection.appendChild(wordOfTheDayTextField);
-
-  //Add turtle image
-  let turtleImage = document.createElement('img');
-  turtleImage.src = '/cdn/images/tortoise.png';
-  turtleImage.classList.add('word-section-turtle');
-  wordSection.appendChild(turtleImage);
-
-  hostBoardContainer.appendChild(wordSection);
-
-  hostBoard.appendChild(hostBoardContainer);
-}
-
-function createStartGameButton() {
-  let hostBoardContainer = document.querySelector('.host-board-container');
-  let hostBoard = document.querySelector('.main-board');
-
-  let startGameButton = document.createElement('button');
-  startGameButton.id = 'btn-start-game';
-  startGameButton.classList.add('start-game-button');
-  startGameButton.innerText = 'Start Game';
-
-  startGameButton.addEventListener('click', () => {
-    const _word = document.getElementById('word-of-the-day').value;
-    socket.emit('data', JSON.stringify({
-      type: 'start_game',
-      username: username,
-      isHost: true,
-      word: _word,
-      roomcode: roomcode
-    }));
-  });
-
-  hostBoardContainer.appendChild(startGameButton);
-  hostBoard.appendChild(hostBoardContainer);
-}
+document.getElementById('btn-start-game').addEventListener('click', () => {
+  const _word = document.getElementById('word-of-the-day').value;
+  socket.emit('data', JSON.stringify({
+    type: 'start_game',
+    username: username,
+    isHost: true,
+    word: _word,
+    roomcode: roomcode
+  }));
+});
